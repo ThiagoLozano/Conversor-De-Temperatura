6daea8331322ba713aceeb7779b3880efa981aeb
unknown <thiagolozano58@gmail.com>
2020-09-09 10:15:13 -0300

    JS

--- functions/function_index.js
+++ functions/function_index.js
@@ -12,6 +12,7 @@ function Validar() {
     // Caso valor não seja um número.
     else if (isNaN(vg)) {
         alert("Erro: Tipo de Dado Inválido!")
+        document.getElementById('valor_graus').value = "";
     }
     else {
         // Caso esteja tudo certo.
